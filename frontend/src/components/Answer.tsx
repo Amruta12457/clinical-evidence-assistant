52f2a8a73c72119a67a3a11373b72c9ec68c1c83
Amruta Killampalli <ksavarshini@gmail.com>
2026-08-17 13:01:04 -0400

    Update Answer.tsx

--- frontend/src/components/Answer.tsx
+++ frontend/src/components/Answer.tsx
@@ -1,6 +1,9 @@
 import type { AnswerProps } from "../types"
 
 function Answer({ answer } : AnswerProps) {
+    if (!answer) {
+        return null
+    }
     return (
         <>
             <p>{answer}</p>
